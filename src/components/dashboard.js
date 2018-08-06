24bc2a3a11364d5b3284622e21514a20ac804767
Srey Seng <sreyseng@gmail.com>
2018-08-06 14:10:30 -0700

    DashboardListItem: turn item into component

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,36 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import Card from '@material-ui/core/Card';
-import Button from '@material-ui/core/Button';
-import Avatar from '@material-ui/core/Avatar';
-import { Typography } from '../../node_modules/@material-ui/core';
-import Grid from '@material-ui/core/Grid';
+
+import DashboardListItem from './leaderboard_list_item';
 
 const styles = (theme) => ({
-  cardAvatar: {
-    width: 100,
-    height: 100,
-    margin: 10
-  },
   tabs: {
     marginBottom: theme.spacing.unit
-  },
-  root: {
-    margin: 'auto',
-    flexGrow: 1,
-    maxWidth: 600,
-    padding: theme.spacing.unit * 2
-  },
-  button: {
-    margin: 10
-  },
-  cardHeading: {
-    padding: theme.spacing.unit,
-    backgroundColor: theme.palette.grey[200],
-    textAlign: 'left'
   }
 });
 
@@ -50,7 +28,7 @@ class Dashboard extends Component {
   render() {
     const { classes } = this.props;
     return (
-      <div>
+      <Fragment>
         <Tabs
           className={classes.tabs}
           indicatorColor="primary"
@@ -61,35 +39,8 @@ class Dashboard extends Component {
           <Tab label="Unanswered Questions" />
           <Tab label="Answered Questions" />
         </Tabs>
-
-        <div className={classes.root}>
-          <Card>
-            <Grid container direction="row" justify="space-around" alignItems="center">
-              <Grid item xs={12} className={classes.cardHeading}>
-                <Typography gutterBottom variant="subheading">
-                  Tyler McGinnis Asks:
-                </Typography>
-              </Grid>
-              <Grid item xs={3}>
-                <Avatar className={classes.cardAvatar} alt="complex" src="/default_04.jpeg" />
-              </Grid>
-              <Grid item xs={9}>
-                <Typography gutterBottom variant="body2">
-                  Would you rather...
-                </Typography>
-                <Typography color="textSecondary">
-                  be a software engineer or be a teacher?
-                </Typography>
-                <Typography align="right">
-                  <Button variant="outlined" className={classes.button}>
-                    View Poll
-                  </Button>
-                </Typography>
-              </Grid>
-            </Grid>
-          </Card>
-        </div>
-      </div>
+        <DashboardListItem />
+      </Fragment>
     );
   }
 }
---
+++ src/components/leaderboard_list_item.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import Button from '@material-ui/core/Button';
+import Avatar from '@material-ui/core/Avatar';
+import { Typography } from '../../node_modules/@material-ui/core';
+import Grid from '@material-ui/core/Grid';
+
+const styles = (theme) => ({
+  cardAvatar: {
+    width: 100,
+    height: 100,
+    margin: 10
+  },
+  tabs: {
+    marginBottom: theme.spacing.unit
+  },
+  root: {
+    margin: 'auto',
+    flexGrow: 1,
+    maxWidth: 600,
+    padding: theme.spacing.unit * 2
+  },
+  button: {
+    margin: 10
+  },
+  cardHeading: {
+    padding: theme.spacing.unit,
+    backgroundColor: theme.palette.grey[200],
+    textAlign: 'left'
+  }
+});
+
+class DashboardListItem extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      defaultTab: 0
+    };
+  }
+
+  handleTabChange(event, defaultTab) {
+    this.setState({ defaultTab });
+  }
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <div className={classes.root}>
+        <Card>
+          <Grid container direction="row" justify="space-around" alignItems="center">
+            <Grid item xs={12} className={classes.cardHeading}>
+              <Typography gutterBottom variant="subheading">
+                Tyler McGinnis Asks:
+              </Typography>
+            </Grid>
+            <Grid item xs={3}>
+              <Avatar className={classes.cardAvatar} alt="complex" src="/default_04.jpeg" />
+            </Grid>
+            <Grid item xs={9}>
+              <Typography gutterBottom variant="body2">
+                Would you rather...
+              </Typography>
+              <Typography color="textSecondary">be a software engineer or be a teacher?</Typography>
+              <Typography align="right">
+                <Button variant="outlined" className={classes.button}>
+                  View Poll
+                </Button>
+              </Typography>
+            </Grid>
+          </Grid>
+        </Card>
+      </div>
+    );
+  }
+}
+
+DashboardListItem.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(DashboardListItem);
