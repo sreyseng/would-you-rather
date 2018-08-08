12b94747af0182360a47340814c2c5efaa227d25
Srey Seng <sreyseng@gmail.com>
2018-08-07 23:37:19 -0700

    Cleanup - logs

--- src/actions/index.js
+++ src/actions/index.js
@@ -69,9 +69,6 @@ export function handleGetUsers(callback) {
   return (dispatch) => {
     return _getUsers().then((users) => {
       dispatch(recieveUsers(users));
-
-      //todo: REMOVE - auto login during dev
-      // dispatch(handleAuthenticateUser('tylermcginnis'));
     });
   };
 }
